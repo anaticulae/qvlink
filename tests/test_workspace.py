e16a5f64fd8a9dcb45a2635dd87e537d72b4c301
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2020-09-27 08:25:30 +0200

    style(imports): organize imports

--- tests/test_workspace.py
+++ tests/test_workspace.py
@@ -8,21 +8,17 @@
 # =============================================================================
 
 import os
-from os.path import exists
 
 import configo
 import utila
 
-import link.workspace
-from link import create_todo
-from link import find_free_todo
-from tests import MINIMAL
-from tests import patch_todo
+import link
+import tests
 
 
 def test_free_todo(tmpdir, monkeypatch):
-    """Generate the same random name twice. This happens not very often, but
-    it can happen."""
+    """Generate the same random name twice. This happens not very often,
+    but it can happen."""
 
     folder = iter(['test', 'test', 'ts'])
 
@@ -37,28 +33,28 @@ def test_free_todo(tmpdir, monkeypatch):
         context.setattr(configo, 'todo', todo)
 
         # Create folder name, so the next call is already created
-        os.makedirs(os.path.join(tmpdir, find_free_todo()))
+        os.makedirs(os.path.join(tmpdir, link.find_free_todo()))
 
         # test is already there, give me the next one
-        assert find_free_todo() == 'ts'
+        assert link.find_free_todo() == 'ts'
 
 
 def test_create_todo(tmpdir, monkeypatch):
-    with patch_todo(tmpdir, monkeypatch):
+    with tests.patch_todo(tmpdir, monkeypatch):
 
         class SaveMock:
 
             def save(self, _):  # pylint:disable=W0613
                 pass
 
-        created = create_todo(SaveMock(), 'testfile.pdf')
-    assert exists(created), created
+        created = link.create_todo(SaveMock(), 'testfile.pdf')
+    assert os.path.exists(created), created
 
 
 def test_create_todo_pathandname(testdir):
     root = str(testdir)
-    created = create_todo(MINIMAL, 'testfile.pdf', root, 'helmut')
-    assert exists(created), created
+    created = link.create_todo(tests.MINIMAL, 'testfile.pdf', root, 'helmut')
+    assert os.path.exists(created), created
 
 
 def test_sortable_date():
@@ -89,5 +85,5 @@ def test_sortable_date():
         '31.11.2019 12:12',
     ]
 
-    result = sorted(result, key=link.workspace.sortable_date, reverse=True)
+    result = sorted(result, key=link.sortable_date, reverse=True)
     assert result == expected
