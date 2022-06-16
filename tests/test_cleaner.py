8b6e843fdbcea8accbcfb8dc608e970509b02fc5
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-06-16 12:18:54 +0200

    test(cleaner): verify job cleaner

---
+++ tests/test_cleaner.py
@@ -0,0 +1,22 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import utilatest
+
+import link
+import tests.patch
+
+
+def test_remove_outdated(common, monkeypatch, capsys):
+    """Ensure that `broken` is cleaned by remover."""
+    with tests.patch.patch_todo(common, monkeypatch):
+        link.remove_outdated()
+    stderr = utilatest.stdout(capsys)
+    # broken is compleated by `remove_outdated`
+    assert 'complete/fail: broken' in stderr
